8a904cee51300977d90561b422f24470e8b78f40
FirstSymbol <firstsymbol.0@gmail.com>
2025-12-30 02:09:12 +0300

    Update colors

---
+++ Assets/_ProjectContext/Scripts/Infrastructure/Configs/Log/ColorTagColorsConfig.cs
@@ -0,0 +1,17 @@
+﻿using ExtDebugLogger.Attributes;
+using ExtInspectorTools;
+using Infrastructure.Services.Input;
+using Infrastructure.StateMachines;
+using UnityEngine;
+
+namespace _ProjectContext.Scripts.Infrastructure.Configs.Log
+{
+  [CreateAssetMenu(fileName = "ColorTagColors Config", menuName = "Configs/ColorTags/ColorTagColors", order = 0)]
+  public class ColorTagColorsConfig : ScriptableObject
+  {
+    [field: SerializeField, ExtDebugLoggerTags]
+    public SerializableDictionary<StateMachineLogTag, Color> stateMachinesColors { get; private set;}
+    [field: SerializeField, ExtDebugLoggerTags]
+    public SerializableDictionary<InputServiceLogTag, Color> inputServiceColors { get; private set;}
+  }
+}
