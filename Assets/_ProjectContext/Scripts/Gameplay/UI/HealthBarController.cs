f8417b9ec5e7de889dad3936f2f8f18308c4eb88
FirstSymbol <firstsymbol.0@gmail.com>
2026-02-11 20:52:12 +0300

    instant spawn health

--- Assets/_ProjectContext/Scripts/Gameplay/UI/HealthBarController.cs
+++ Assets/_ProjectContext/Scripts/Gameplay/UI/HealthBarController.cs
@@ -13,23 +13,28 @@ namespace Scripts.Gameplay.UI
     public float sliderValue = 1.38f;
     private void OnEnable()
     {
-      creatureHealth.OnTakeDamage += UpdateHealth;
+      creatureHealth.OnTakeDamage += TakeDamage;
     }
 
     private void OnDisable()
     {
-      creatureHealth.OnTakeDamage -= UpdateHealth;
+      creatureHealth.OnTakeDamage -= TakeDamage;
     }
 
     private void Start()
     {
-      UpdateHealth();
+      UpdateHealth(0f);
     }
 
-    private void UpdateHealth()
+    private void TakeDamage()
+    {
+      UpdateHealth(0.2f);
+    }
+
+    private void UpdateHealth(float duration)
     {
       var t = (float)creatureHealth.HealthValue / creatureHealth.MaxHealthValue;
-      LMotion.Create(rectMask.padding.z, sliderValue - (sliderValue * t), 0.2f)
+      LMotion.Create(rectMask.padding.z, sliderValue - (sliderValue * t), duration)
         .Bind(v => rectMask.padding = new Vector4(0, 0, v));
 
     }
